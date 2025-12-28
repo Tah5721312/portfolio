7671077462dce23f46deb8279228c1a51284c06a
tah5721312 <tah5721312@gmail.com>
2025-12-28 19:55:25 +0200

    tah14

--- src/services/data/projects.ts
+++ src/services/data/projects.ts
@@ -4,7 +4,7 @@ export const projects = [
     title: 'Clinic-app',
     description:
       'A modern clinic management application built with Next.js App Router, React 18, and TypeScript. Features a beautiful UI with Tailwind CSS 3, comprehensive testing with Jest, and a complete development workflow including ESLint, Prettier, Husky, and GitHub Actions. Includes automatic sitemap generation, Open Graph optimization, and follows conventional commit standards for maintainable code.',
-    technologies: ['Next.js', 'React', 'TypeScript', 'Tailwind CSS 3', 'Jest', 'ESLint', 'Prettier', 'Husky', 'GitHub Actions'],
+    technologies: ['Next.js', 'React','PostgreSQL', 'TypeScript', 'Tailwind CSS 3', 'Jest', 'ESLint', 'Prettier', 'Husky', 'GitHub Actions'],
     image: '/images/clinic.png',
     demoLink: 'https://clinic-app-prisma-postgre.vercel.app',
     githubLink: 'https://github.com/Tah5721312/clinic-app-prisma-postgre',
@@ -14,7 +14,7 @@ export const projects = [
     title: 'Inventory',
     description:
       'A full-stack inventory platform featuring a Next.js App Router frontend powered by React 19.1.0, TypeScript 5.8.3, Tailwind CSS 4.1.11, and Lucide React icons. The backend relies on PostgreSQL with Prisma 5.22.0, Next.js API routes, and a secure auth layer using NextAuth.js 5.0, CASL, and bcryptjs. Data validation, HTTP interactions, and user feedback are handled with Zod, Axios, and React Toastify respectively for end-to-end reliability.',
-    technologies: ['Next.js','React','TypeScript','Tailwind CSS','Lucide React','PostgreSQL','Prisma','Next.js API Routes','NextAuth.js','CASL','bcryptjs','Zod','Axios','React Toastify',],
+    technologies: ['Next.js','React','PostgreSQL','TypeScript','Tailwind CSS','Lucide React','PostgreSQL','Prisma','Next.js API Routes','NextAuth.js','CASL','bcryptjs','Zod','Axios','React Toastify',],
     image: '/images/inventory.png',
     demoLink: 'https://inventory-prisma-postgre.vercel.app/',
     githubLink: 'https://github.com/Tah5721312/inventory-prisma-postgre',
@@ -23,8 +23,8 @@ export const projects = [
     id: '3',
     title: 'E-commerce',
     description:
-      'A full-stack E-commerce platform featuring a Next.js App Router frontend powered by React 19.1.0, TypeScript 5.8.3, Tailwind CSS 4.1.11, and Lucide React icons. The backend relies on PostgreSQL with Prisma , Next.js API routes, and a secure auth layer using NextAuth.js , CASL, and bcryptjs. Data validation, HTTP interactions, and user feedback are handled with Zod, Axios, and React Toastify respectively for end-to-end reliability.',
-    technologies: ['Next.js','React','TypeScript','Tailwind CSS','Lucide React','PostgreSQL','Prisma 5.22.0','Next.js API Routes','NextAuth.js 5.0','CASL','bcryptjs','Zod','Axios','React Toastify',],
+      'A full-stack E-commerce platform featuring a Next.js App Router frontend powered by React 19.1.0, TypeScript 5.8.3, Tailwind CSS 4.1.11, and Lucide React icons. The backend relies on PostgreSQL with Prisma 5.22.0, Next.js API routes, and a secure auth layer using NextAuth.js 5.0, CASL, and bcryptjs. Data validation, HTTP interactions, and user feedback are handled with Zod, Axios, and React Toastify respectively for end-to-end reliability.',
+    technologies: ['Next.js','React','PostgreSQL','TypeScript','Tailwind CSS','Lucide React','PostgreSQL','Prisma 5.22.0','Next.js API Routes','NextAuth.js 5.0','CASL','bcryptjs','Zod','Axios','React Toastify',],
     image: '/images/ecommerce.png',
     demoLink: 'https://e-commerce-all.vercel.app/',
     githubLink: 'https://github.com/Tah5721312/e-commerce-all',
@@ -33,8 +33,8 @@ export const projects = [
     id: '4',
     title: 'gps-tracking',
     description:
-      'A full-stack gps-tracking platform featuring a Next.js App Router frontend powered by React, TypeScript , Tailwind CSS, and Lucide React icons. The backend relies on PostgreSQL with Prisma , Next.js API routes, and a secure auth layer using NextAuth.js , CASL, and bcryptjs. Data validation, HTTP interactions, and user feedback are handled with Zod, Axios, and React Toastify respectively for end-to-end reliability.',
-    technologies: ['Next.js','React','TypeScript','Tailwind CSS','Lucide React','PostgreSQL','Prisma 5.22.0','Next.js API Routes','NextAuth.js 5.0','CASL','bcryptjs','Zod','Axios','React Toastify',],
+      'A full-stack gps-tracking platform featuring a Next.js App Router frontend powered by React, TypeScript , Tailwind CSS, and Lucide React icons. The backend relies on PostgreSQL with Prisma 5.22.0, Next.js API routes, and a secure auth layer using NextAuth.js 5.0, CASL, and bcryptjs. Data validation, HTTP interactions, and user feedback are handled with Zod, Axios, and React Toastify respectively for end-to-end reliability.',
+    technologies: ['Next.js','React','PostgreSQL','TypeScript','Tailwind CSS','Lucide React','PostgreSQL','Prisma 5.22.0','Next.js API Routes','NextAuth.js 5.0','CASL','bcryptjs','Zod','Axios','React Toastify',],
     image: '/images/gps-tracking.png',
     demoLink: 'https://gps-tracking-three.vercel.app/',
     githubLink: 'https://github.com/Tah5721312/gps-tracking',
